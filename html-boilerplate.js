d573db6985e55cc7933f954a104da145df784940
Brett Zamir <brettz9@yahoo.com>
2013-06-13 14:51:47 +0800

    allow "top" and "bottom" properties in HTML boilerplate; jslint HTML boilerplate

--- html-boilerplate.js
+++ html-boilerplate.js
@@ -1,18 +1,21 @@
+/*globals module*/
+
 var boilerplate = function (details) {
+    'use strict';
 
     return '<!DOCTYPE html>\n' +
-'<html xmlns="http://www.w3.org/1999/xhtml">\n' +
-'<head>\n' +
-'    <meta charset="utf-8" />\n' +
-'    <title>'+(details.title || '')+'</title>\n' +
-(details.head || '') +
-'</head>\n' +
-'<body>\n' +
-(details.body || '') +
-'</body>\n' +
-'</html>';
-
+        '<html xmlns="http://www.w3.org/1999/xhtml">\n' +
+        '<head>\n' +
+        '    <meta charset="utf-8" />\n' +
+        '    <title>'+(details.title || '')+'</title>\n' +
+        (details.head || '') +
+        '</head>\n' +
+        '<body>\n' +
+        (details.top || '') +
+        (details.body || '') +
+        (details.bottom || '') +
+        '</body>\n' +
+        '</html>';
 };
-
 
 module.exports = boilerplate;
